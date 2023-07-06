8f7b760a45aeade7a36cc765c287581ea9812fb8
cw-shuuzoutouyama <syuuzou.touyama.zh@nttcom.co.jp>
2023-07-06 02:23:43 -0400

    Refactor

--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -4,7 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.beans.Transient;
+
 public class ShippingCalculatorTest {
 
     // @todo: add tests
+    @Transient
+    public void canCalculateTheCostForARegion() {
+
+        ShippingCalculator calculator = new ShippingCalculator();
+
+        assertEquals(0, calculator.costForRegion("A Region"));
+
+    }
 }
